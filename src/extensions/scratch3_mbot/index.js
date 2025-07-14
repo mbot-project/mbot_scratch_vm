9e673db660ee77b57b448fcb01c98bfa1072da7a
Tyler Simon <tyler.simon811@gmail.com>
2025-07-14 16:50:51 -0400

    fix(mbot): dedbug drive commands

--- src/extensions/scratch3_mbot/index.js
+++ src/extensions/scratch3_mbot/index.js
@@ -6,34 +6,38 @@ const { MBotAPI } = require('./dist/main.js');
 const menuIconURI = "";
 const blockIconURI = "";
 
-const mbotIP = window.location.host.split(":")[0];
+const mbotIP = window.location.hostname;
 
 const FT_TO_M = 0.3048;
 const DEG_TO_RAD = Math.PI / 180;
 
-class Scratch3MBot
-{
+class Scratch3MBot {
     mbot;
     mbot_odom;
     mbot_scan;
+    loopRunning = false;
+    stopLoop = false;
 
-    constructor(runtime)
-    {
+    constructor(runtime) {
         this.runtime = runtime;
         this.connectToServer();
     }
 
     connectToServer() {
         this.mbot = new MBotAPI.MBot(mbotIP);
-        this.mbot.readHostname().then((hostname) => { console.log("hostname:", hostname); });
-        this.mbot.readChannels().then((chs) => { console.log("chs:", chs); });
+        this.mbot.readHostname().then(hostname => console.log('hostname:', hostname));
+        this.mbot.readChannels().then(chs => console.log('chs:', chs));
         this.mbot.drive(0, 0, 0);
 
-        this.mbot.subscribe(MBotAPI.config.ODOMETRY.channel, (odom) => { this.mbot_odom = odom; });
-        this.mbot.subscribe(MBotAPI.config.LIDAR.channel, (scan) => { this.mbot_scan = scan; });
+        this.mbot.subscribe(MBotAPI.config.ODOMETRY.channel, odom => {
+            this.mbot_odom = odom;
+        });
+        this.mbot.subscribe(MBotAPI.config.LIDAR.channel, scan => {
+            this.mbot_scan = scan;
+        });
     }
 
-    getInfo () {
+    getInfo() {
         return {
             id: 'mbot',
             name: formatMessage({
@@ -41,229 +45,162 @@ class Scratch3MBot
                 default: 'MBot extension',
                 description: 'Name of the MBot extension.'
             }),
-            blockIconURI: blockIconURI,
-            menuIconURI: menuIconURI,
+            blockIconURI,
+            menuIconURI,
             blocks: [
                 {
-                    opcode: 'drive',
+                    opcode: 'driveTo',
                     text: formatMessage({
-                        id: 'mbot.driveBlock',
-                        default: 'drive( x: [VX], y: [VY], ω: [WZ] )',
+                        id: 'mbot.driveToBlock',
+                        default: 'drive to x: [X], y: [Y]',
                         description: 'Drive the MBot'
                     }),
                     blockType: BlockType.COMMAND,
                     arguments: {
-                        VX: {
-                            type: ArgumentType.NUMBER,
-                            defaultValue: 0.0
-                        },
-                        VY: {
-                            type: ArgumentType.NUMBER,
-                            defaultValue: 0.0
-                        },
-                        WZ: {
-                            type: ArgumentType.NUMBER,
-                            defaultValue: 0.0
-                        }
+                        X: { type: ArgumentType.NUMBER, defaultValue: 0.0 },
+                        Y: { type: ArgumentType.NUMBER, defaultValue: 0.0 }
                     }
                 },
                 {
-                    opcode: 'stop',
+                    opcode: 'driveDirection',
                     text: formatMessage({
-                        id: 'mbot.stopBlock',
-                        default: 'stop MBot',
-                        description: 'Stop the MBot.'
+                        id: 'mbot.driveDirectionBlock',
+                        default: 'drive to the [DIRECTION] at [SPEED] m/s',
+                        description: 'Drive the MBot forward'
                     }),
-                    blockType: BlockType.COMMAND
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        DIRECTION: { type: ArgumentType.STRING, menu: 'direction', defaultValue: 'front' },
+                        SPEED:     { type: ArgumentType.NUMBER, defaultValue: 0.0 }
+                    }
                 },
                 {
                     opcode: 'detectObstacle',
                     text: formatMessage({
                         id: 'mbot.detectObstacleBlock',
-                        default: 'is an obstacle within [DIST] meters at [ANGLE] degrees?',
+                        default: 'detect obstacle at [ANGLE] degrees within [DIST] meters',
                         description: 'Checks for an obstacle in the surrounding area'
                     }),
                     blockType: BlockType.BOOLEAN,
                     arguments: {
-                        DIST: {
-                            type: ArgumentType.NUMBER,
-                            defaultValue: 0.5,
-                            description: 'Distance in meters to check for obstacles.'
-                        },
-                        ANGLE: {
-                            type: ArgumentType.NUMBER,
-                            defaultValue: 0,
-                            description: 'Angle in degrees to check for obstacles. 0 is straight ahead, 90 is to the left, 180 is behind, 270 is to the right.'
-                        }
+                        DIST:  { type: ArgumentType.NUMBER, defaultValue: 0.5 },
+                        ANGLE: { type: ArgumentType.NUMBER, defaultValue: 0 }
                     }
                 },
                 {
-                    opcode: 'angleToNearestObstacle',
+                    opcode: 'driveArc',
                     text: formatMessage({
-                        id: 'mbot.angleToNearestObstacle',
-                        default: 'Angle to nearest obstacle',
-                        description: 'Return the angle to the nearest obstacle.'
-                    }),
-                    blockType: BlockType.REPORTER,
-                    arguments: {}
-                },
-                {
-                    opcode: 'distanceToNearestObstacle',
-                    text: formatMessage({
-                        id: 'mbot.distanceToNearestObstacle',
-                        default: 'Distance to nearest obstacle',
-                        description: 'Return the distance to the nearest obstacle.'
-                    }),
-                    blockType: BlockType.REPORTER,
-                    arguments: {}
-                },
-                {
-                    opcode: 'resetPosition',
-                    text: formatMessage({
-                        id: 'mbot.resetPositionBlock',
-                        default: 'Reset position',
-                        description: 'Reset the position of the MBot.'
+                        id: 'mbot.driveArcBlock',
+                        default: 'drive arc to the [ARCDIRECTION] at [VX] m/s with radius [RADIUS] m',
+                        description: 'Drive the MBot in an arc.'
                     }),
                     blockType: BlockType.COMMAND,
-                    arguments: {}
+                    arguments: {
+                        ARCDIRECTION: { type: ArgumentType.STRING, menu: 'arcdirection', defaultValue: 'left' },
+                        VX:           { type: ArgumentType.NUMBER, defaultValue: 0.5 },
+                        RADIUS:       { type: ArgumentType.NUMBER, defaultValue: 1.0 }
+                    }
                 },
                 {
-                    opcode: `getXPosition`,
+                    opcode: 'driveDirectionForDist',
                     text: formatMessage({
-                        id: 'mbot.getXPositionBlock',
-                        default: 'X Position',
-                        description: 'Get the x position of the MBot in meters.'
+                        id: 'mbot.driveDirectionforDistBlock',
+                        default: 'drive [DIRECTION] [DIST] meters',
+                        description: 'Drive in a straight line for a distance.'
                     }),
-                    blockType: BlockType.REPORTER,
-                    arguments: {}
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        DIRECTION: { type: ArgumentType.STRING, menu: 'direction', defaultValue: 'front' },
+                        DIST:      { type: ArgumentType.NUMBER, defaultValue: 1.0 }
+                    }
                 },
+                { opcode: 'stop', text: formatMessage({id:'mbot.stopBlock', default:'stop MBot'}), blockType:BlockType.COMMAND },
+                { opcode: 'angleToNearestObstacle',   text: formatMessage({id:'mbot.angleToNearestObstacle', default:'Angle to nearest obstacle'}),   blockType:BlockType.REPORTER },
+                { opcode: 'distanceToNearestObstacle',text: formatMessage({id:'mbot.distanceToNearestObstacle', default:'Distance to nearest obstacle'}),blockType:BlockType.REPORTER },
                 {
-                    opcode: `getYPosition`,
+                    opcode: 'detectObstacleInDirection',
                     text: formatMessage({
-                        id: 'mbot.getYPositionBlock',
-                        default: 'Y Position',
-                        description: 'Get the y position of the MBot in meters.'
+                        id: 'mbot.detectObstacleInDirectionBlock',
+                        default: 'detect obstacle [DIRECTION] within [DIST] meters'
                     }),
-                    blockType: BlockType.REPORTER,
-                    arguments: {}
+                    blockType: BlockType.BOOLEAN,
+                    arguments: {
+                        DIRECTION: { type: ArgumentType.STRING, menu: 'direction', defaultValue: 'front' },
+                        DIST:      { type: ArgumentType.NUMBER, defaultValue: 0.5 }
+                    }
                 },
-                {
-                    opcode: `getHeading`,
-                    text: formatMessage({
-                        id: 'mbot.getHeadingBlock',
-                        default: 'Heading',
-                        description: 'Get the heading of the MBot in degrees.'
-                    }),
-                    blockType: BlockType.REPORTER,
-                    arguments: {}
-                },
-                // {
-                //     opcode: `predictNumber`,
-                //     text: formatMessage({
-                //         id: 'mbot.predictNumberBlock',
-                //         default: 'number from image',
-                //         description: 'Get the number in view of the camera.'
-                //     })
-                // }
+                { opcode: 'resetPosition',   text: formatMessage({id:'mbot.resetPositionBlock', default:'Reset position'}),   blockType:BlockType.COMMAND },
+                { opcode: 'getXPosition',    text: formatMessage({id:'mbot.getXPositionBlock', default:'X Position'}),    blockType:BlockType.REPORTER },
+                { opcode: 'getYPosition',    text: formatMessage({id:'mbot.getYPositionBlock', default:'Y Position'}),    blockType:BlockType.REPORTER },
+                { opcode: 'getHeading',      text: formatMessage({id:'mbot.getHeadingBlock', default:'Heading'}),      blockType:BlockType.REPORTER }
             ],
             menus: {
-                angle: {
-                    acceptReporters: true,
-                    items: [
-                        {
-                            text: formatMessage({
-                                id: 'mbot.angleLeft',
-                                default: 'left',
-                                description: 'Left direction.'
-                            }),
-                            value: 'left'
-                        },
-                        {
-                            text: formatMessage({
-                                id: 'mbot.angleRight',
-                                default: 'right',
-                                description: 'Right direction.'
-                            }),
-                            value: 'right'
-                        }
-                    ]
-                },
                 direction: {
                     acceptReporters: true,
                     items: [
-                        {
-                            text: formatMessage({
-                                id: 'mbot.dirFront',
-                                default: 'front',
-                                description: 'Front direction.'
-                            }),
-                            value: 'front'
-                        },
-                        {
-                            text: formatMessage({
-                                id: 'mbot.dirBack',
-                                default: 'back',
-                                description: 'Back direction.'
-                            }),
-                            value: 'back'
-                        },
-                        {
-                            text: formatMessage({
-                                id: 'mbot.dirLeft',
-                                default: 'left',
-                                description: 'Left direction.'
-                            }),
-                            value: 'left'
-                        },
-                        {
-                            text: formatMessage({
-                                id: 'mbot.dirRight',
-                                default: 'right',
-                                description: 'Right direction.'
-                            }),
-                            value: 'right'
-                        }
+                        { text: formatMessage({id:'mbot.dirFront', default:'front'}), value:'front' },
+                        { text: formatMessage({id:'mbot.dirBack', default:'back'}),   value:'back'  },
+                        { text: formatMessage({id:'mbot.dirLeft', default:'left'}),   value:'left'  },
+                        { text: formatMessage({id:'mbot.dirRight',default:'right'}),  value:'right' }
+                    ]
+                },
+                arcdirection: {
+                    acceptReporters: true,
+                    items: [
+                        { text: formatMessage({id:'mbot.dirLeft', default:'left'}),  value:'left'  },
+                        { text: formatMessage({id:'mbot.dirRight',default:'right'}), value:'right' }
                     ]
                 }
             }
         };
     }
 
-    drive (args) {
-        const vx = args.VX * 1.0;
-        const vy = args.VY * 1.0;
-        const wz = args.WZ * DEG_TO_RAD;
-        this.mbot.drive(vx, vy, wz);
-    }
-    
-    stop () {
-        this.mbot.drive(0, 0, 0);
-    }
-    
-    detectObstacle(args){
-        if (this.mbot_scan == null) {
-            return false;
-        }
-        
-        var dir = args.ANGLE * DEG_TO_RAD
+    driveTo(args) {
+        const Kp = 0.5;
+        const goalX = args.X * 1.0 + this.mbot_odom.data.x;
+        const goalY = args.Y * 1.0 + this.mbot_odom.data.y;
+        let distance_prev = 0;
 
-        if (dir === undefined) {
-            return false;
-        }
-
-        const dist = args.DIST
-        const slice_size = 30 * DEG_TO_RAD
-
-        for (let i = 0; i < this.mbot_scan.data.ranges.length; i++) {
-            var range = this.mbot_scan.data.ranges[i]
-            var theta = this.mbot_scan.data.thetas[i]
-            if (range < dist && range > 0 && Math.abs(theta - dir) < slice_size) {
-                return true
+        const interval = setInterval(() => {
+            this.loopRunning = true;
+            if (this.stopLoop) {
+                this.loopRunning = false;
+                this.stopLoop = false;
+                clearInterval(interval);
+                return;
             }
-        }
-        return false
+            if (!this.mbot_odom?.data) return;
+            const dx = goalX - this.mbot_odom.data.x;
+            const dy = goalY - this.mbot_odom.data.y;
+            const distance = Math.hypot(dx, dy);
+            
+            if (Math.abs(distance - distance_prev) < 0.001) {
+                this.loopRunning = false;
+                this.stopLoop = false;
+                clearInterval(interval);
+                return;
+            }
+
+            distance_prev = distance;
+            const vx = Math.min(Kp * dx, 0.5);
+            const vy = Math.min(Kp * dy, 0.5);
+            this.mbot.drive(vx, vy, 0);
+            
+        }, 100);
     }
-    
+
+    driveDirection(args) {
+        const speed = args.SPEED * 1.0;
+        let vx = 0, vy = 0;
+        switch (args.DIRECTION) {
+            case 'front': vx = speed; break;
+            case 'back':  vx = -speed; break;
+            case 'left':  vy = speed; break;
+            case 'right': vy = -speed; break;
+        }
+        this.mbot.drive(vx, vy, 0);
+    }
+
     angleToNearestObstacle () {
         if (this.mbot_scan == null) {
             return 0;
@@ -297,25 +234,63 @@ class Scratch3MBot
         return min_range
     }
 
-    resetPosition () {
-        this.mbot.publish(MBotAPI.config.RESET_ODOMETRY.channel, { x: 0, y: 0, theta: 0 });
+    detectObstacle(args) {
+        if (!this.mbot_scan?.data) return false;
+        if (typeof args.ANGLE !== 'number') return false;
+        const dir = args.ANGLE * DEG_TO_RAD;
+        const dist = args.DIST * 1.0;
+        const sliceSize = 30 * DEG_TO_RAD;
+        for (let i = 0; i < this.mbot_scan.data.ranges.length; i++) {
+            const range = this.mbot_scan.data.ranges[i];
+            const theta = this.mbot_scan.data.thetas[i];
+            if (range > 0 && range < dist && Math.abs(theta - dir) < sliceSize) return true;
+        }
+        return false;
     }
 
-    getXPosition () {
-        return this.mbot_odom.data.x;
+    driveArc(args) {
+        const vx = args.VX * 1.0;
+        let radius = args.RADIUS * 1.0;
+        if (args.ARCDIRECTION === 'right') radius = -radius;
+        const omega = vx / radius;
+        this.mbot.drive(vx, 0, omega);
     }
 
-    getYPosition () {
+    driveDirectionForDist(args) {
+        const distance = args.DIST * 1.0;
+        switch (args.DIRECTION) {
+            case "front":
+                this.driveTo({"X": distance, "Y": 0});
+                break;
+            case "left":
+                this.driveTo({"X": 0, "Y": distance});
+                break;
+            case "right":
+                this.driveTo({"X": 0, "Y": -distance});
+                break;
+            case "back":
+                this.driveTo({"X": -distance, "Y": 0});
+                break;
+            default:
+                return;
+        }
+    }
+        
+    stop() {
+        if (this.loopRunning) {
+            this.stopLoop = true;
+        }
+        this.mbot.drive(0, 0, 0);
+    }
+
+    getYPosition() {
         return this.mbot_odom.data.y;
-    } 
+    }
 
-    getHeading () {
+    getHeading() {
         return this.mbot_odom.data.theta * 180 / Math.PI;
     }
-
-    // predictNumber () {
-    // }
 }
-
+    
 
 module.exports = Scratch3MBot;
